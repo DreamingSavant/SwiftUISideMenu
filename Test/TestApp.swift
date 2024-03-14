b77617961741a5b1766f5517fad1e28a43b34e59
DreamingSavant <rodpwood@gmail.com>
2024-03-14 11:36:38 -0400

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Roderick Presswood on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
